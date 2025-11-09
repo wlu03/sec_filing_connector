1e108611eb9e655e0dd6bc6b324d6fc21b20d9f3
ByteManSagnik <sagniksinha12@gmail.com>
2025-11-10 00:45:04 +0400

    Created the __init__py file

--- sec_connector/sec_connector/__init__.py
+++ sec_connector/sec_connector/__init__.py
@@ -1,7 +1,4 @@
-"""SEC EDGAR filing connector."""
-
 from sec_connector.models import Company, Filing, FilingFilter
 from sec_connector.client import SECClient
-
 __version__ = "0.1.0"
 __all__ = ["Company", "Filing", "FilingFilter", "SECClient"]
